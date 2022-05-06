757bede56dbbb46d9fe7c559dadd7b3201116cb6
AmandaCacholi <amanda.cacholi@outlook.com>
2022-05-05 22:19:47 -0300

    feat: mostrar input quando marcado

--- src/cadastro-login/cadastro-login.js
+++ src/cadastro-login/cadastro-login.js
@@ -1,9 +1,9 @@
-document.getElementById("checkbox").addEventListener("click");
+document.getElementById("checkbox").addEventListener("click", showInput);
 
 function showInput() {
   const checkbox = document.getElementById("checkbox");
   const cnpjInput = document.getElementById("checkbox-cnpj-input");
-  console.log(checkbox);
+
   if (checkbox.checked == true) {
     cnpjInput.style.display = "block";
   } else {
