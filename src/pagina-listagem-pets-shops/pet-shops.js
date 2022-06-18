4c3d5f7f05dbbc1687a7768647f3d5475b92f68b
AmandaCacholi <amanda.cacholi@outlook.com>
2022-06-18 18:09:41 -0300

    feat: ajuste listagem pet shops

--- src/pagina-listagem-pets-shops/pet-shops.js
+++ src/pagina-listagem-pets-shops/pet-shops.js
@@ -1,3 +1,19 @@
+const loggedUser = JSON.parse(localStorage.getItem("login"));
+
+if (loggedUser) {
+  document.getElementById("header-content-login").style.display = "none";
+  document.getElementById("header-my-information").style.display = "flex";
+  document
+    .getElementById("header-content-logo-link-logo")
+    .setAttribute("href", "../pagina-inicial/pagina-inicial.html");
+  document
+    .getElementById("header-content-logo-link-title")
+    .setAttribute("href", "../pagina-inicial/pagina-inicial.html");
+} else {
+  document.getElementById("header-content-login").style.display = "flex";
+  document.getElementById("header-my-information").style.display = "none";
+}
+
 const registers = JSON.parse(localStorage.getItem("registered"));
 const petShops = registers.filter((item) => item.service === "pet-shop");
 
@@ -12,3 +28,9 @@ let adinionaCardPet = (data) => {
 petShops.forEach((element) => {
   adinionaCardPet(element);
 });
+
+document.getElementById("header-logout").addEventListener("click", logout);
+
+function logout() {
+  localStorage.removeItem("login");
+}
