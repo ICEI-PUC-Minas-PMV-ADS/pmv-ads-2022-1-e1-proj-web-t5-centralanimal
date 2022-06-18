9ed35d99eddd943da78bfa2d7dafceb39faeac9e
AmandaCacholi <amanda.cacholi@outlook.com>
2022-06-18 17:31:02 -0300

    feat? ajuste de ongs

--- src/pagina-listagem-das-ongs/ongs.js
+++ src/pagina-listagem-das-ongs/ongs.js
@@ -1,3 +1,25 @@
+const loggedUser = JSON.parse(localStorage.getItem("login"));
+
+if (loggedUser) {
+  document.getElementById("header-content-login").style.display = "none";
+  document.getElementById("header-my-information").style.display = "flex";
+  document
+    .getElementById("header-content-logo-link-logo")
+    .setAttribute("href", "../pagina-inicial/pagina-inicial.html");
+  document
+    .getElementById("header-content-logo-link-title")
+    .setAttribute("href", "../pagina-inicial/pagina-inicial.html");
+} else {
+  document.getElementById("header-content-login").style.display = "flex";
+  document.getElementById("header-my-information").style.display = "none";
+}
+
+document.getElementById("header-logout").addEventListener("click", logout);
+
+function logout() {
+  localStorage.removeItem("login");
+}
+
 const registers = JSON.parse(localStorage.getItem("registered"));
 const ongs = registers.filter((item) => item.service === "ONG");
 
