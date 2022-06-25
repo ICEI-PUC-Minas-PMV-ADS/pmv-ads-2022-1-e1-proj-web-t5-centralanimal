d92c5858536a4473c47316dad5418543b07820ee
AmandaCacholi <amanda.cacholi@outlook.com>
2022-06-25 19:24:04 -0300

    feat: adicionar comentarios em pet hotel

--- src/pagina-listagem-hoteis-para-pets/pet-hotel.js
+++ src/pagina-listagem-hoteis-para-pets/pet-hotel.js
@@ -41,24 +41,108 @@ const companyAddress = document.getElementById("modal-enterprise-address");
 const companyImage = document.getElementById("modal-enterprise-image");
 const companyDocument = document.getElementById("modal-enterprise-document");
 
+let orgId;
+let orgComments = [];
+
 function getCardId(event) {
   const id = event.target.id;
-  const item = registers.filter((item) => item.id === id)[0];
+  orgId = id;
 
-  console.log(item);
-  companyName.innerText = item.name;
-  companyDescription.innerText = item.description;
-  companyPhone.innerText = `Telefone: ${item.phoneNumber}`;
-  companyEmail.innerText = `Email: ${item.email}`;
-  companyAddress.innerText = `Endereço: ${item.address.address}, ${
-    item.address.number
-  } - ${item.address.complement ? `${item.address.complement}.` : ""} ${
-    item.address.neighborhood
-  }, ${item.address.city} - ${item.address.state} ${item.address.country} - ${
-    item.address.zipCode
+  const org = registers.filter((item) => item.id === id)[0];
+  orgComments = org.comments;
+
+  companyName.innerText = org.name;
+  companyDescription.innerText = org.description;
+  companyPhone.innerText = `Telefone: ${org.phoneNumber}`;
+  companyEmail.innerText = `Email: ${org.email}`;
+  companyAddress.innerText = `Endereço: ${org.address.address}, ${
+    org.address.number
+  } - ${org.address.complement ? `${org.address.complement}.` : ""} ${
+    org.address.neighborhood
+  }, ${org.address.city} - ${org.address.state} ${org.address.country} - ${
+    org.address.zipCode
   }`;
-  companyImage.setAttribute("src", item.image);
-  companyDocument.innerText = item.cnpj;
+  companyImage.setAttribute("src", org.image);
+  companyDocument.innerText = org.cnpj;
+
+  if (orgComments.length) {
+    org.comments.reverse().map((item) => {
+      let li = document.createElement("li");
+
+      li.innerHTML = `
+      <div class="comment-card">
+      <p class="comment-title">${item.name} - ${item.date.slice(
+        0,
+        10
+      )} - Nota: ${item.score}</p>
+      <p class="comment-comment">${item.comment}</p>
+      </div>
+      `;
+
+      document.getElementById("comment-list").appendChild(li);
+    });
+  }
+}
+
+// Comentário
+
+const formComment = document.getElementById("comment-form");
+document.getElementById("comment-form").addEventListener("submit", saveComment);
+
+function saveComment(event) {
+  event.preventDefault();
+
+  const comment = document.getElementById("comment-input").value;
+  const score = document.getElementById("organization-scores").value;
+
+  const data = {
+    comment,
+    score: parseInt(score),
+    date: new Date(),
+    name: loggedUser.name,
+    userId: loggedUser.id,
+    id: Date.now() + Math.random().toFixed(),
+  };
+
+  const org = registers.filter((item) => item.id === orgId)[0];
+  const newOrgInfo = { ...org, comments: [...org.comments, data] };
+
+  const currentUserInfo = {
+    ...loggedUser,
+    comments: [...loggedUser.comments, data],
+  };
+
+  const alreadyCommented = org.comments
+    .map((item) => item.userId)
+    .includes(loggedUser.id);
+
+  if (!alreadyCommented) {
+    orgComments.push(data);
+
+    const orgIndex = registers.map((item) => item.id).indexOf(orgId);
+    const currentUserIndex = registers
+      .map((item) => item.id)
+      .indexOf(loggedUser.id);
+
+    registers.splice(orgIndex, 1, newOrgInfo);
+    registers.splice(currentUserIndex, 1, currentUserInfo);
+
+    const newRegisters = registers;
+    localStorage.setItem("registered", JSON.stringify(newRegisters));
+    localStorage.setItem("login", JSON.stringify(currentUserInfo));
+
+    alert("Avaliação postada!");
+    document.location.reload();
+  } else {
+    alert("Vocẽ já deixou uma avaliação para essa organização!");
+    formComment.reset();
+  }
+}
+
+document.getElementById("btn-close").addEventListener("click", resetModal);
+
+function resetModal() {
+  document.location.reload();
 }
 
 document.getElementById("header-logout").addEventListener("click", logout);
@@ -66,17 +150,3 @@ document.getElementById("header-logout").addEventListener("click", logout);
 function logout() {
   localStorage.removeItem("login");
 }
-
-let lines = gets().split("\n");
-var N = parseInt(lines.shift());
-
-function gets(N) {
-  for (let i = 1; i <= N; i++) {
-    if (i % 2 === 0) {
-      let R = i * i;
-      console.log(`${i}^2 = ${R}`);
-    }
-  }
-}
-
-gets(5);
